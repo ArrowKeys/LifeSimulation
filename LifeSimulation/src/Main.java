1c9408200f0495739176ae9885867ae009a3571d
punym <punym@DESKTOP-QBQ76GJ>
2019-01-01 16:54:12 -0500

    Instantiated Player and Game objects

--- LifeSimulation/src/Main.java
+++ LifeSimulation/src/Main.java
@@ -2,8 +2,12 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
-
+		Player plr = new Player();
+		Game game = new Game();
+		
+		while (plr.isAlive()) {
+			game.showMenu();
+		}
 	}
 
 }
