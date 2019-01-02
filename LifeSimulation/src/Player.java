338119e84794d5d4cd895eb4ac84eb8de219b590
punym <punym@DESKTOP-QBQ76GJ>
2019-01-01 19:06:46 -0500

    Added more names

--- LifeSimulation/src/Player.java
+++ LifeSimulation/src/Player.java
@@ -1,9 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Player {
-	
-	private final int MAX_AGE = 100; 
-	private final int MIN_AGE = 0; 
+
+	private final int MAX_AGE = 100;
+	private final int MIN_AGE = 0;
 
 	private Random r;
 
@@ -12,28 +16,50 @@ public class Player {
 	private int age;
 	private boolean alive;
 
-	private String[] firstNames = { };
-	private String[] lastNames = { };
+	private ArrayList<String> firstNames = new ArrayList<String>();
+	private ArrayList<String> lastNames = new ArrayList<String>();
 
 	public Player() {
+
+		try {
+			String firstNamesFile = "Names/FirstNames";
+			String lastNamesFile = "Names/LastNames";
+			firstNames = populateNames(firstNamesFile);
+			lastNames = populateNames(lastNamesFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 		this.r = new Random();
 		this.age = 0;
 		this.alive = true;
-		this.firstName = firstNames[r.nextInt(firstNames.length)];
-		this.lastName = lastNames[r.nextInt(lastNames.length)];
+		this.firstName = firstNames.get(r.nextInt(firstNames.size()));
+		this.lastName = lastNames.get(r.nextInt(lastNames.size()));
+
+	}
+
+	private ArrayList<String> populateNames(String fileName) throws FileNotFoundException {
+
+		ArrayList<String> names = new ArrayList<String>();
+
+		Scanner sc = new Scanner(new File(fileName));
+		while (sc.hasNextLine()) {
+			names.add(sc.nextLine());
+		}
+		sc.close();
+		return names;
 
 	}
 
 	public boolean isAlive() {
-
 		return ((this.getAge() >= this.MIN_AGE && this.getAge() < this.MAX_AGE) && this.getAlive());
 	}
-	
+
 	public void setAlive(boolean alive) {
 		this.alive = alive;
 	}
 
-	public boolean getAlive() {		
+	public boolean getAlive() {
 		return alive;
 	}
 
